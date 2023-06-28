9863e8f10603adc70a3137003e8e8948b0d5a64f
= <ardayildirim05@icloud.com>
2023-06-28 15:58:16 +0200

    Fix- insert navbar to the ProtectedRoutes component

--- src/pages/ProtectedRoutes.tsx
+++ src/pages/ProtectedRoutes.tsx
@@ -1,12 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { getUser } from "../hooks/useAuth";
+import NavBar from "../components/NavBar";
 
 const ProtectedRoutes = () => {
   const user = getUser();
 
   if (!user) return <Navigate to="/login" />;
 
-  return <Outlet />;
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
 };
 
 export default ProtectedRoutes;
